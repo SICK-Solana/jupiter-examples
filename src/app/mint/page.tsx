c57896cfc4bc1e1409d352dd4743801792579624
Vighnesh Shenekar <115893429+vighnxsh@users.noreply.github.com>
2024-09-15 14:18:01 +0530

    ..

---
+++ src/app/mint/page.tsx
@@ -0,0 +1,16 @@
+import {
+    mintTo,
+    createMint,
+    createAssociatedTokenAccount,
+    getAssociatedTokenAddress,
+  } from "@solana/spl-token";
+
+const Mint = () => {
+    return (
+        <div>
+            <h1>Mint</h1>
+        </div>
+    )
+}
+
+export default Mint;
